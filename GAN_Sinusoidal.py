0556346800db6dafc17665f99bca527a4d3bd080
ryi2022 <112025539+ryi2022@users.noreply.github.com>
2022-09-27 14:11:10 -0400

    Update GAN_Sinusoidal.py

--- GAN_Sinusoidal.py
+++ GAN_Sinusoidal.py
@@ -7,7 +7,7 @@ class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(2, 128),
+            nn.Linear(2, 256),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(256, 128),
